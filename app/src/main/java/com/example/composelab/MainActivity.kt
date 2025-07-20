649a09c305b2c427345a74b8e729cf3983311869
Meezzi <alswl-3016@naver.com>
2025-07-20 15:15:22 +0900

    feat: Navigation Rail 구현

--- app/src/main/java/com/example/composelab/MainActivity.kt
+++ app/src/main/java/com/example/composelab/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import com.example.composelab.ui.basics.BasicsScreen
+import com.example.composelab.ui.layout.MySootheAppPortrait
 import com.example.composelab.ui.theme.ComposeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeLabTheme {
-                BasicsScreen(modifier = Modifier.fillMaxSize())
+//                BasicsScreen(modifier = Modifier.fillMaxSize())
+                MySootheAppPortrait()
             }
         }
     }
